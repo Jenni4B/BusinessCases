6537bb3f87605cff461edecd1f20564cc279d654
Jenni4B <jennifer.ea.blocker@gmail.com>
2024-10-23 14:53:39 -0400

    Centered login section box, created the dasboard file, added a navigation bar, login & logout button. Need to add data validation to the username and password but the idea works

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 
+// Account functions and variables
+
+let accountInfo = {
+    name: "Jenni4B",
+    username: "",
+    password: "",
+    twoStep: "",
+}
+
+function logIn(){}
+
+function logOut(){}
+
+function logInValidation(){
+
+    // If the fields match inside accountInfo:
+    //      logIn(){}
+
+    // else:
+    //      "User name, password, or twoStepAuth is incorrect"
+
+}
+
+function createAccount(){}
+
+
+// dashboard.html
+
+let welcome = document.getElementById('welcome')
+
+welcome.textContent = `Welcome ${accountInfo.name}!`;
